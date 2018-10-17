463edae0402b17659462269829ad0ce862e431b5
alexisbranlant <Internet4952>
2018-10-17 15:01:33 +0200

    Proposition d'optimisation d'une partie du code

--- Base2048.js
+++ Base2048.js
@@ -1,9 +1,9 @@
-(function($) 
+(function($)
 {
     var score = 0;
     var size = 4;
 
-    $.fn.game2048 = function() 
+    $.fn.game2048 = function()
     {
         function generate_map()
         {
@@ -13,12 +13,12 @@
             for (var y = 0; y < size; y++){
                 var ligne = $('<tr></tr>');
                 for (var x = 0; x < size; x++) {
-                    var cases = $('<td id = "cell">0</td>').attr('x', x).attr('y', y).attr('nbr', 0).attr('merge', 0).css({"background-color": "CadetBlue", "width": "112px", "height": "112px", "border-radius": "25px", "font-size": "50px", "text-align": "center", "font-family": "'Arial'", "font-weight": "bold"});
+                    var cases = $('<td id="cell">0</td>').attr('x', x).attr('y', y).attr('nbr', 0).attr('merge', 0);
                     ligne.append(cases);
                 }
                 table.append(ligne);
-                borda.append(table);
             }
+            borda.append(table);//pas besoin d'injecter le tableau dans chaque tour de boucle
             return borda;
         }
 
@@ -27,7 +27,7 @@
             var head = $('<div><h1>2048</h1></div>').css({"text-align": "center", "font-size": "32px", "font-family": "'Arial'", "color": "DarkSlateGrey"});
             var instr = $('<div><p class="instr"> Use your arrow keys to move the tiles. When two tiles with the same number touch, they merge into one!</p> </div>').css({"font-size": "19px", "padding": "5px", "margin-left": "15%", "margin-right": "15%", "margin-bottom": "10px", "font-family": "Arial", "background-color": "#f6f6f6"});
             var set = $('<div style= "display: inline-flex"><input style= "width: 100px; height: 50px" type="button" value="New game" onclick="window.location.reload()"> <p id="score" style="width: 100px; height: 50px; color:white; background-color: DarkSlateGrey; font-weight: bold; font-size:22px; margin:0px; vertical-align:bottom">Score</p></div>');
-          
+
             instr.append(set);
             head.append(instr);
 
@@ -42,39 +42,45 @@
                 var y = Math.floor((Math.random() * size));
 
                 var value =  2 * (Math.floor((Math.random() * 2) + 1));
-                
+
                 var elem = $('[x="' + x + '"][y="' + y + '"][nbr=0]');
 
                 if (value === 4 && Math.random() > 0.5)
                     value = 2;
-                
+
                 if (!elem[0])
                     generate_case(cases - i);
-                
+
                 else {
                     elem.attr('nbr', value);  //// 2 or 4 as value (nbr) in cell
-                    elem.text(value).css({"font-size": "50px", "text-align": "center"});  ///displays value
-                    setColor();
+                    elem.text(value);//.css({"font-size": "50px", "text-align": "center"});  ///displays value
+                    //setColor();
                 }
             }
         }
 
         function reset_merge(){
-        //back to attr merge = 0 out of loop
+            //back to attr merge = 0 out of loop
 
+
+            // ???????? l'objectif est de mettre toutes les cases a merge=0
+            // pourquoi une boucle dans une boucle !?
+            /*
             for(var y = 0; y <= (size-1); y++){
-
                 for(var x = 0; x <= (size-1); x++){
 
                     var arrival = $('[x = "'+x+'"][y = "'+y+'"]');
+                    console.log(arrival);
                     arrival.attr('merge', 0);
                 }
             }
+            */
+            $('td').attr('merge', 0);
         }
 
 
         function moveLeft(){
-          
+
             reset_merge();
 
             let move = false;
@@ -93,7 +99,7 @@
                         var value_arrival = parseInt(arrival.attr('nbr'));
                         ///// identif value 'nbr'
 
-                        var merge = parseInt(depart.attr('merge')) || parseInt(arrival.attr('merge'));   
+                        var merge = parseInt(depart.attr('merge')) || parseInt(arrival.attr('merge'));
 
                         if(value_depart === value_arrival && merge !== 1 && value_arrival !== 0 && value_depart !== 0){
                             //console.log("condition1");
@@ -121,7 +127,7 @@
         }
 
         function moveRight(){
-            
+
             reset_merge();
 
             let move = false;
@@ -138,7 +144,7 @@
                         var value_depart = parseInt(depart.attr('nbr'));
                         var value_arrival = parseInt(arrival.attr('nbr'));
 
-                        var merge = parseInt(depart.attr('merge')) || parseInt(arrival.attr('merge'));   
+                        var merge = parseInt(depart.attr('merge')) || parseInt(arrival.attr('merge'));
 
                         if(value_depart === value_arrival && merge !== 1 && value_arrival !== 0 && value_depart !== 0){
                             arrival.text(value_depart + value_arrival);
@@ -170,7 +176,7 @@
 
             let move = false;
 
-            for(var x = 0; x <= (size-1); x++){ 
+            for(var x = 0; x <= (size-1); x++){
 
                 for(var y = (size-1); y >= 0; y--){
 
@@ -182,7 +188,7 @@
                         var value_depart = parseInt(depart.attr('nbr'));
                         var value_arrival = parseInt(arrival.attr('nbr'));
 
-                        var merge = parseInt(depart.attr('merge')) || parseInt(arrival.attr('merge'));   
+                        var merge = parseInt(depart.attr('merge')) || parseInt(arrival.attr('merge'));
 
                         if(value_depart === value_arrival && merge !== 1 && value_arrival !== 0 && value_depart !== 0){
                             arrival.text(value_depart + value_arrival);
@@ -191,7 +197,7 @@
                             depart.text('0');
                             arrival.attr('merge', 1);
                             move = true;
-                            score += parseInt(mergeValue.attr('nbr'));                  
+                            score += parseInt(mergeValue.attr('nbr'));
                         }
 
                         else if(value_depart !== value_arrival && value_arrival === 0){
@@ -205,7 +211,7 @@
                 }
             }
 
-            return move;       
+            return move;
         }
 
 
@@ -215,7 +221,7 @@
 
             let move = false;
 
-            for(var x = 0; x <= (size-1); x++){ 
+            for(var x = 0; x <= (size-1); x++){
 
                 for(var y = 0; y <= (size-1); y++){
 
@@ -224,7 +230,7 @@
                         var depart = $('[x = "'+x+'"][y = "'+tmp+'"]');
                         var arrival = $('[x = "'+x+'"][y = "'+(tmp-1)+'"]');
 
-                        var merge = parseInt(depart.attr('merge')) || parseInt(arrival.attr('merge'));   
+                        var merge = parseInt(depart.attr('merge')) || parseInt(arrival.attr('merge'));
 
                         var value_depart = parseInt(depart.attr('nbr'));
                         var value_arrival = parseInt(arrival.attr('nbr'));
@@ -236,7 +242,7 @@
                             depart.text('0');
                             arrival.attr('merge', 1);
                             move = true;
-                            score += parseInt(mergeValue.attr('nbr'));                 
+                            score += parseInt(mergeValue.attr('nbr'));
                         }
 
                         else if(value_depart !== value_arrival && value_arrival === 0){
@@ -246,13 +252,14 @@
                             depart.text('0');
                             move = true;
                         }
-                    }    
+                    }
                 }
             }
             return move;
         }
 
-        function setColor(){      
+        /* faisable en css
+        function setColor(){
             $('td').each(function(){
                 if(parseInt($(this).attr('nbr')) === 0) {$(this).css({"background-color": "CadetBlue", "color": "transparent"});}
                 if(parseInt($(this).attr('nbr')) === 2) {$(this).css({"background-color": "rgb(95, 128, 160)", "color": "#F0F0F0"});}
@@ -268,11 +275,11 @@
                 if(parseInt($(this).attr('nbr')) === 2048) {$(this).css({"background-color": "rgb(255, 102, 102)", "color": "blue"});}
              });
         }
-
+        */
 
         $('html').keydown(function(event) {
             let a;
-        
+
             switch (event['key']) {
                 case 'ArrowLeft':
                     a = moveLeft();
@@ -294,17 +301,20 @@
                 generate_case(1);
             }
 
-            setColor();
+            //setColor();
             $('#score').text(score);
             setWin();
             gameOver();
         });
 
- 
+        // ?????
+        // Pourquoi ne pas dire "c'est gagné" directement quand tu fais le "mergeValue" ?
+        // ou mettre en mémoire qu'il y a un 2048
+        // Ici tu parcours le dom pour savoir si c'est gagné, niveau perf c'est moyen :/
         function setWin(){
 
-            var win;
-            
+            var win; // ??? A quoi sert la variable ?
+
             $('td').each(function(){
                 if(parseInt($(this).attr('nbr')) === 2048){
                     setTimeout(function(){
@@ -312,11 +322,11 @@
                         $('td').attr('nbr', 0).text('0');
                         generate_map();
                         generate_case(2);
-                        }, 120);                                      
+                    }, 120);
                 }
-            });   
+            });
         }
-        
+
 
         function gameOver(){
 
@@ -352,16 +362,16 @@
             }
 
             setTimeout(function(){
-            alert("YOU SUCK!");
-            $('td').attr('nbr', 0).text('0');
-            generate_map();
-            generate_case(2);
-            }, 120); 
-                   
+                alert("YOU SUCK!");
+                $('td').attr('nbr', 0).text('0');
+                generate_map();
+                generate_case(2);
+            }, 120);
+
         }
 
         $(this).append(displayStructure());
-        setColor();
+        //setColor();
         $(this).append(generate_map());
         generate_case(2);
     }
